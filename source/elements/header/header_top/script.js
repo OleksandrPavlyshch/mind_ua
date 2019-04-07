e5d8770f384a9e1dc9e881f51efba7c45ea7b40e
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-04-07 09:33:05 +0300

    add search

--- source/elements/header/header_top/script.js
+++ source/elements/header/header_top/script.js
@@ -1,10 +1,23 @@
 $(function(){
 	var $body = $('body')
 		, $menuButton = $('.header_new-menu_button')
-		, menuShowClass = 'is-menu-show';
+		, menuShowClass = 'is-menu-show'
+		, $searchInput = $('.header_new-search-input')
+		, focusClass = 'is_focused'
+		, $searchForm = $('.header_new-search');
 
 	$menuButton.on('click', function() {
 		$menuButton.toggleClass('active');
 		$body.toggleClass(menuShowClass);
-	})
+	});
+
+	$searchInput.focus(function() {
+		console.log('dd')
+		$searchForm.addClass(focusClass);
+	});
+	$searchInput.blur(function() {
+		$searchForm.removeClass(focusClass);
+	});
+
+
 });
