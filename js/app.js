a14533100d2e02d58b5fb2da9c063ed01e73e003
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-04-10 00:08:45 +0300

    Update 2019-04-09T21:08:43.892Z

--- js/app.js
+++ js/app.js
@@ -116,8 +116,12 @@ $(function(){
 	menuCollapsToDorpdown($botMenuList, $botMenuDropdawn, $botMenuDropdawnButton);
 	menuCollapsToDorpdown($topMenuList, $topMenuDropdawn, $topMenuDropdawnButton);
 });
-
-
+$(function(){
+	$('.header_new-open-section_header').click(function(event) {
+		$(this).toggleClass('is_open')
+		.next('.header_new-open-list').slideToggle();
+	});
+});
 $(function(){
 	var $body = $('body')
 		, $menuButton = $('.header_new-menu_button')
