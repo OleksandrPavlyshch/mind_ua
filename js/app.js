3cf3d64c3730661ed172f1eda0d4c223088192de
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-04-10 08:47:24 +0300

    Update 2019-04-10T05:47:22.987Z

--- js/app.js
+++ js/app.js
@@ -117,6 +117,18 @@ $(function(){
 
 $(function(){
 	var $body = $('body')
+		, $menuButton = $('.header_new-menu_button');
+	$('.header_new-open-section_header').click(function(event) {
+		$(this).toggleClass('is_open')
+		.next('.header_new-open-list').slideToggle();
+	});
+	$('.header_new-open-button_close').on('click', function() {
+		$menuButton.removeClass('active');
+		$body.removeClass('is-menu-show');
+	});
+});
+$(function(){
+	var $body = $('body')
 		, $menuButton = $('.header_new-menu_button')
 		, menuShowClass = 'is-menu-show'
 		, $searchInput = $('.header_new-search-input')
@@ -170,16 +182,4 @@ $(function(){
 		}
 	});
 
-});
-$(function(){
-	var $body = $('body')
-		, $menuButton = $('.header_new-menu_button');
-	$('.header_new-open-section_header').click(function(event) {
-		$(this).toggleClass('is_open')
-		.next('.header_new-open-list').slideToggle();
-	});
-	$('.header_new-open-button_close').on('click', function() {
-		$menuButton.removeClass('active');
-		$body.removeClass('is-menu-show');
-	});
 });
