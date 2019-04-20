90be166dd644009e32fbe2be989f7dff4ea67e1c
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-04-20 03:39:17 +0300

    add menu markup

--- source/elements/footer/script.js
+++ source/elements/footer/script.js
@@ -2,4 +2,8 @@ $(function(){
 	$('.footer_new-arrow_up').click(function() {
 		$('html, body').animate({ scrollTop: 0 });
 	});
+	$('.footer_new-section_header').click(function(event) {
+		$(this).toggleClass('is_open')
+		.next('.footer_new-list').slideToggle();
+	});
 })
