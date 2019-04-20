2b9fe71801c6bd7c848ca90ea0aa8a32d17881ba
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-04-20 04:18:07 +0300

    add menu

--- source/elements/footer/script.js
+++ source/elements/footer/script.js
@@ -2,8 +2,11 @@ $(function(){
 	$('.footer_new-arrow_up').click(function() {
 		$('html, body').animate({ scrollTop: 0 });
 	});
+
 	$('.footer_new-section_header').click(function(event) {
-		$(this).toggleClass('is_open')
-		.next('.footer_new-list').slideToggle();
+		if ($(window).width() <= 1024) {
+			$(this).toggleClass('is_open')
+			.next('.footer_new-list').slideToggle();
+		}
 	});
 })
--- source/elements/header/header_open/script.js
+++ source/elements/header/header_open/script.js
@@ -1,10 +1,14 @@
 $(function(){
 	var $body = $('body')
 		, $menuButton = $('.header_new-menu_button');
+
 	$('.header_new-open-section_header').click(function(event) {
-		$(this).toggleClass('is_open')
-		.next('.header_new-open-list').slideToggle();
+		if ($(window).width() <= 1024){
+			$(this).toggleClass('is_open')
+			.next('.header_new-open-list').slideToggle();
+		}
 	});
+
 	$('.header_new-open-button_close').on('click', function() {
 		$menuButton.removeClass('active');
 		$body.removeClass('is-menu-show');
