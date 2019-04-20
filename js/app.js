4f8193819d6d3a1fbeafe51e3b8117401b41fa93
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-04-20 04:13:16 +0300

    Update 2019-04-20T01:13:15.532Z

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@ $(function(){
 	$('.footer_new-arrow_up').click(function() {
 		$('html, body').animate({ scrollTop: 0 });
 	});
+
 	$('.footer_new-section_header').click(function(event) {
-		$(this).toggleClass('is_open')
-		.next('.footer_new-list').slideToggle();
+		if ($(window).width() <= 1024) {
+			$(this).toggleClass('is_open')
+			.next('.footer_new-list').slideToggle();
+		}
 	});
 })
 
@@ -135,20 +138,23 @@ $(function(){
 	menuCollapsToDorpdown($topMenuList, $topMenuDropdawn, $topMenuDropdawnButton);
 	menuCollapsToDorpdown($stickyMenuList, $stickyMenuDropdawn, $stickyMenuDropdawnButton);
 });
+
 $(function(){
 	var $body = $('body')
 		, $menuButton = $('.header_new-menu_button');
+
 	$('.header_new-open-section_header').click(function(event) {
-		$(this).toggleClass('is_open')
-		.next('.header_new-open-list').slideToggle();
+		if ($(window).width() <= 1024){
+			$(this).toggleClass('is_open')
+			.next('.header_new-open-list').slideToggle();
+		}
 	});
+
 	$('.header_new-open-button_close').on('click', function() {
 		$menuButton.removeClass('active');
 		$body.removeClass('is-menu-show');
 	});
 });
-
-
 $(function(){
 	var $body = $('body')
 		, $menuButton = $('.header_new-menu_button')
