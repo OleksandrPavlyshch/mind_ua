d4fcaa2463a8fa6a02602cad8e956eb373ede746
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2019-06-10 15:49:51 +0300

    Update 2019-06-10T12:49:49.886Z

--- js/app.js
+++ js/app.js
@@ -138,6 +138,8 @@ $(function(){
 	menuCollapsToDorpdown($topMenuList, $topMenuDropdawn, $topMenuDropdawnButton);
 	menuCollapsToDorpdown($stickyMenuList, $stickyMenuDropdawn, $stickyMenuDropdawnButton);
 });
+
+
 $(function () {
     function validateEmail(email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
